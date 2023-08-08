a55bb51936a265cfb3e42c6e1637a80c198cebd0
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2023-08-08 01:28:23 -0300

    Criando a rota de eventos

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { Application } from 'express';
 import { connect } from './infra/database';
 import { errorMiddleware } from './middlewares/error.middleware';
+import { EventRoutes } from './routes/event.routes';
 
 class App {
 
     public app: Application;
+
+    private eventRoutes = new EventRoutes();
+
     constructor() {
         this.app = express();
         this.middlewaresInitialize();
@@ -14,7 +18,7 @@ class App {
     }
 
     initializeRoutes() {
-        //this.app.use('/', )
+        this.app.use('/events', this.eventRoutes.router)
     }
 
     interceptionError() {
